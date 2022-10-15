ff080ee0f9d9caf990b9d75944b8623649af6bc0
Aleksey <aleksejbaskirov31@gmail.com>
2022-10-16 01:40:56 +0300

    Done Diplom AND Do Archive Part of Project

--- DiplomProject-app_COPY_3/shop/index/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('index/basket-add/<int:product_id>', views.basket_add, name="basket_add"),
-]
--- DiplomProject_COPY_9_DetailView_ContactForm/DiplomProject-app/shop/contact_form/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import contact  # ContactCreate, success_page
-
-urlpatterns = [
-    # path('', ContactCreate.as_view(), name='contact'),
-    # path('success/', success_page, name="success_page"),
-    path('contact/', contact, name='contact'),
-]
